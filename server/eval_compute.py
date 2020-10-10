6421d7c525af0fe4b1f2e7dbae87048af33387db
root <root@VM1141-vm01.ustc.edu.cn>
2020-10-10 08:53:55 +0000

    fix a bug

--- server/eval_compute.py
+++ server/eval_compute.py
@@ -8,10 +8,11 @@ def signal_handler(signum , frame):
 def eval_com(stri):
     signal.signal(signal.SIGALRM, signal_handler)
     try:
-        signal.alarm(3)   # Ten seconds
+        signal.alarm(5)   # Ten seconds
         result = eval(stri)
         print(result)
         signal.alarm(0)
     except:
+        signal.alarm(0)
         return '太烧脑了'
     return result
