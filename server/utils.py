2b7bae1fe32026188f57950ed6fa70d5eb732680
root <root@VM1141-vm01.ustc.edu.cn>
2020-10-15 13:14:50 +0000

    add a rss source

--- server/utils.py
+++ server/utils.py
@@ -11,7 +11,11 @@ import time
 from qa import *
 from poker import *
 import feedparser
-
+import requests
+proxies = {
+    'http': 'http://127.0.0.1:8889',
+    'https': 'https://127.0.0.1:8889',
+}
 
 class BOT:
     def __init__(self , data):
@@ -21,19 +25,62 @@ class BOT:
         self.answer = QA(data['answer'])
         self.tar_url = data['tar_url']
         self.rss_list = data['rss_list']
+        self.search_rss = [[re.compile(r'动漫花园(.*)'),'https://dmhy.anoneko.com/topics/rss/rss.xml?keyword=','&sort_id=2&team_id=0&order=date-desc'], 
+        [re.compile(r'arxiv(.*)'),'https://rsshub.app/arxiv/search_query=all:','&start=0&max_results=10'],
+        [re.compile(r'华尔街见闻(.*)'),'https://rsshub.app/weexcn/news/1',''],
+        [re.compile(r'3DM(.*)'),'https://rsshub.app/3dm/news',''],
+        #[re.compile(r'谷歌学术(.*)'),'','']
+        ]
         #self.texas_num = 0
 
     def process(self , jresp):
+
+        search_str_k = self.in_search_rss(jresp['message'])
         if jresp['message'] in self.rss_list:
             try:
 
                 rss = feedparser.parse(self.rss_list[jresp['message']])
                 result = ''
-                for i in range(0,5):
-                    result += rss.entries[i].title+'\n'+rss.entries[i].id+'\n'+rss.entries[i].published+'\n\n'
+                cnt = 0
+                for i in rss.entries:
+                    if cnt >= 5:
+                        break
+                    if i.title not in result:
+                        result += i.title+'\n'+i.id+'\n'+i.published+'\n\n'
+                        cnt += 1
                 self.send_group_msg(result,jresp['group_id'])
             except:
                 print('error')
+
+
+        elif search_str_k!=-1:
+            try:
+                
+                select = self.search_rss[search_str_k][0].findall(jresp['message'])
+                #print(anime_select)
+                result = ''
+                print(select)
+                if select == [] or search_str_k in [2,3]:
+                    select.append('')
+                if search_str_k==0:
+                    rss = feedparser.parse(self.search_rss[search_str_k][1]+urllib.parse.quote(select[0])+self.search_rss[search_str_k][2])
+                else:
+                    response = requests.get(self.search_rss[search_str_k][1]+urllib.parse.quote(select[0])+self.search_rss[search_str_k][2], 
+                    proxies = proxies)
+                    rss = feedparser.parse(response.text)
+                cnt = 0
+                for i in rss.entries:
+                    if cnt >= 5:
+                        break
+                    if i.title not in result:
+                        result += i.title+'\n'+i.id+'\n'+i.published+'\n\n'
+                        cnt += 1
+
+                if result == '':
+                    result = '找不到'
+            except:
+                result = '出错了'
+            self.send_group_msg(result,jresp['group_id'])
 
 
         elif jresp['message'] == '开始德州':
@@ -151,3 +198,9 @@ class BOT:
         req = urllib.request.Request(self.tar_url + "/set_restart_plugin",urllib.parse.urlencode({"delay": d}).encode('utf8'))
         rep = urllib.request.urlopen(req).read()
         print(rep)
+
+    def in_search_rss(self,str):
+        for i in range(0,len(self.search_rss)):
+            if self.search_rss[i][0].match(str):
+                return i
+        return -1
